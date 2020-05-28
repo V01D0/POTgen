d41b1ee691055f99f8f367aa1d092594a66a8d11
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2020-05-28 11:08:32 +1000

    Update potgen.py

--- potgen.py
+++ potgen.py
@@ -5,7 +5,6 @@ This script generates .po files for a given .pot file
 @copyright : https://pryv8.org
 """
 
-
 from bs4 import BeautifulSoup
 import re
 from googletrans import Translator
@@ -16,6 +15,12 @@ import fileinput
 r = requests.get("https://cloud.google.com/translate/docs/languages")
 data = r.text
 soup = BeautifulSoup(data,"html.parser")
+
+"""
+* Really needs to get the header from the most recent POT file
+* If POT POT-Creation-Date is later than the PO-Revision-Date this script
+    needs to pull missing msgid's from pot file and merge in the po files
+"""
 
 heading = """
 # SOME DESCRIPTIVE TITLE.
@@ -36,13 +41,14 @@ msgstr ""
 "MIME-Version: 1.0\\n"
 "Content-Type: text/plain; charset=CHARSET\\n"
 "Content-Transfer-Encoding: 8bit\\n"
+"Plural-Forms: nplurals=2; plural=n > 1;\n"
 """
 
 for lang in soup.find_all('code'):
     lang = str(lang.getText())
     filename = lang+".po"
     f2 = open(filename,"wt")
-    now = datetime.datetime.now()
+    now = datetime.datetime.now(datetime.timezone.utc)
     date = str(now.year) + "-" + str(now.month) + "-" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + "+0000"
     f2.write(heading)
     f2.close()
@@ -53,4 +59,5 @@ for lang in soup.find_all('code'):
     data = data.replace('FULL NAME <EMAIL@ADDRESS>', f"Google Translate {lang}@li.org")
     data = data.replace('LANGUAGE <LL@li.org>', f"LANGUAGE <{lang}@li.org>")
     data = data.replace('Language: ', f"Language: {lang}")
+    data = data.replace('CHARSET', f"UTF-8")
     f2.write(data)
