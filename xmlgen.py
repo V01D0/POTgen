f09a93b56e72dbd298e8017f7c87b7c05ad86671
V01D0 <38071738+V01D0@users.noreply.github.com>
2023-01-10 09:41:37 +0530

    changes in string replacement

--- xmlgen.py
+++ xmlgen.py
@@ -11,23 +11,23 @@ import xml.etree.ElementTree as ET
 
 
 file_name = sys.argv[1]
-xml_file  = minidom.parse(sys.argv[1])
+xml_file = minidom.parse(sys.argv[1])
 
 strings = xml_file.getElementsByTagName('string')
 original_texts = {}
 
 FILENAME = Path("google.langs.html")
 if FILENAME.is_file():
-    F2 = open(FILENAME, "r")
-    DATA = F2.read()
-    F2.close()
+	F2 = open(FILENAME, "r")
+	DATA = F2.read()
+	F2.close()
 else:
-    print("google.langs doesn't exist... downloading...")
-    R = requests.get("https://cloud.google.com/translate/docs/languages")
-    DATA = R.text
-    F2 = open(FILENAME, "wt")
-    F2.write(DATA)
-    F2.close()
+	print("google.langs doesn't exist... downloading...")
+	R = requests.get("https://cloud.google.com/translate/docs/languages")
+	DATA = R.text
+	F2 = open(FILENAME, "wt")
+	F2.write(DATA)
+	F2.close()
 
 # print(strings[0].getAttribute("name"))
 # exit()
@@ -37,43 +37,33 @@ for string in strings:
 	name = string.attributes['name'].value
 	print(name)
 	for i in range(child_nodes):
-		original_texts.update({name:string.childNodes[i].data})
+		original_texts.update({name: string.childNodes[i].data})
 		# print(string.childNodes[i].name)
 		# original_texts.append(string.childNodes[i].data)
 
-# print(original_texts)
+print(original_texts)
 
 TRANS = translate.Client()
 SOUP = BeautifulSoup(DATA, "html.parser")
 
 for lang in SOUP.find_all('code'):
-    translated_texts = copy.deepcopy(original_texts)
-    curlang = str(lang.getText())
-    if curlang == 'en' or curlang == 'la' or curlang == 'ceb':
-        continue
-    if curlang == 'zh-CN' or curlang == 'zh-TW':
-        continue
-    current_directory = os.getcwd()
-    values_directory = os.path.join(current_directory, f"values-{curlang}")
-    if not os.path.exists(values_directory):
-        # print(values_directory)
-        os.mkdir(values_directory)
-        with open (file_name) as f:
-            tree = ET.parse(f)
-            root = tree.getroot()
-            for k,v in translated_texts.items():
-                translated_string = TRANS.translate(v,source_language='en',target_language=curlang)
-                strings = xml_file.getElementsByTagName("strings")
-                for elem in root.iter():
-                    try:
-                        elem.text = elem.text.replace(v, translated_string)
-                    except AttributeError:
-                        print("errrrrr")
-            tree.write(f'{values_directory}/strings.xml', encoding='utf-8')
-            # for k,v in translated_texts:
-            #     translated_string = TRANS.translate(v,source_language='en',target_language=curlang)
-            #     strings = xml_file.getElementsByTagName("strings")
-            #     for string in strings:
-            #         if string.getAttribute("name") == k:
-            #             string.childNodes[0].data = translated_string
-        # print(strings.getAttribute("name"))
+	translated_texts = copy.deepcopy(original_texts)
+	curlang = str(lang.getText())
+	if curlang == 'en' or curlang == 'la' or curlang == 'ceb':
+		continue
+	if curlang == 'zh-CN' or curlang == 'zh-TW':
+		continue
+	current_directory = os.getcwd()
+	values_directory = os.path.join(current_directory, f"values-{curlang}")
+	if not os.path.exists(values_directory):
+		os.mkdir(values_directory)
+		with open(file_name) as f:
+			tree = ET.parse(f)
+			root = tree.getroot()
+			for k, v in translated_texts.items():
+				translated_string = TRANS.translate(
+					v, source_language='en', target_language=curlang)
+				for element in root.findall("string"):
+					if element.text == v:
+						element.text = translated_string
+			tree.write(f'{values_directory}/strings.xml', encoding='utf-8')
